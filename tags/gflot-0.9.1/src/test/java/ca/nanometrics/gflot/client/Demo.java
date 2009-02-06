077f00f972db3f676086e46195290081f2840c87
alexjdl <alexjdl@db7342a2-c7c0-11dd-a2c4-6dc3ad32ce87>
2009-02-06 14:50:06 +0000

    [maven-release-plugin]  copy for tag gflot-0.9.1

---
+++ tags/gflot-0.9.1/src/test/java/ca/nanometrics/gflot/client/Demo.java
@@ -0,0 +1,36 @@
+package ca.nanometrics.gflot.client;
+
+import ca.nanometrics.gflot.client.example.GFlotExample;
+import ca.nanometrics.gflot.client.example.HoverExample;
+import ca.nanometrics.gflot.client.example.SimplePlotExample;
+import ca.nanometrics.gflot.client.example.TickFormatterExample;
+
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabPanel;
+
+public class Demo {
+
+	private final TabPanel m_tabs = new TabPanel();
+
+	private void addExamples() {
+		addExample(new SimplePlotExample());
+		addExample(new TickFormatterExample());
+		addExample(new HoverExample());
+	}
+
+	private void addExample(GFlotExample example) {
+		m_tabs.add(example.createExample(), example.getName());
+	}
+
+	public void onModuleLoad() {
+
+		m_tabs.setWidth("100%");
+		m_tabs.setHeight("100%");
+
+		RootPanel.get().add(m_tabs);
+
+		addExamples();
+		m_tabs.selectTab(0);
+	}
+
+}
